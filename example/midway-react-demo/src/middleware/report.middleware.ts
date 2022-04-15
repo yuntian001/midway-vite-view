bc7787bb0a5d1c1d143698e5dcd56bf8115fea7a
HOMEDO\zhangjiayun <zhangjiayun@homedo.com>
2022-04-15 17:24:17 +0800

    no message

--- example/midway-react-demo/src/middleware/report.middleware.ts
+++ example/midway-react-demo/src/middleware/report.middleware.ts
@@ -12,7 +12,7 @@ export class ReportMiddleware implements IMiddleware<Context, NextFunction> {
       // 这里可以拿到下一个中间件或者控制器的返回值
       const result = await next();
       // 控制器之后执行的逻辑
-      console.log(Date.now() - startTime);
+      console.log('ReportMiddleware 中间件',Date.now() - startTime);
       // 返回给上一个中间件的结果
       return result;
     };
