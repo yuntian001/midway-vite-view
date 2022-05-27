c6f079e06958d42ead5f716701d9b58a78a593c3
javaSwing <zxdlovejava@gmail.com>
2022-05-27 19:29:08 +0800

    feat: update react-demo

--- example/midway-react-demo/src/config/config.default.ts
+++ example/midway-react-demo/src/config/config.default.ts
@@ -1,19 +1,15 @@
-import { MidwayConfig } from '@midwayjs/core';
-
 export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1646896633407_6393',
   koa: {
     port: 7001,
   },
-  view: { //midwayjs 视图配置 说明参考 https://midwayjs.org/docs/extensions/render
+  view: {
     defaultViewEngine: 'viteView',
   },
-  viteView: { //midway-vite-view 配置配置详细说明见下方
+  viteView: {
     clientIndex: ['index.html'],
-    entryServers: [
-      'src/entry-server.js',
-    ],
+    entryServers: ['src/entry-server.tsx'],
   },
-} as MidwayConfig;
+};
 
--- example/midway-react-demo/src/controller/home.controller.ts
+++ example/midway-react-demo/src/controller/home.controller.ts
@@ -10,7 +10,7 @@ export class HomeController {
   async home(): Promise<string> {
     //服务端渲染
     return this.ctx.render('index.html', {
-      entry: 'src/entry-server.jsx',
+      entry:  'src/entry-server.tsx' ,
       assign:{title:'vite midway'},//html中{{title}}的会被替换为vite midway
     });
   }
